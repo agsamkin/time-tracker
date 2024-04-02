2789a98f8398077ce04174a0acb8eb70df2f1a58
ags <a.g.samkin@gmail.com>
2024-04-02 16:17:59 +0300

    Fix build.yml

--- src/test/java/com/example/dz1/Dz1ApplicationTests.java
+++ src/test/java/com/example/dz1/Dz1ApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.dz1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Dz1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
